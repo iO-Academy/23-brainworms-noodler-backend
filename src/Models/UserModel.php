b1897c9cd05791fa4e0ab05694575e2fc4a11dac
Mitch <michaelthierytucker@gmail.com>
2023-12-11 12:15:30 +0000

    entities and model added

---
+++ src/Models/UserModel.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace App\Models;
+
+use PDO;
+
+class UserModel
+{
+    private PDO $db;
+
+    /**
+     * Creates UserModel constructor.
+     */
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * Gets email and password from database to login.
+     */
+    public function getUserByEmail($userEmail): array
+    {
+        if ($this->validateEmail($userEmail) !== false) {
+            $query = $this->db->prepare("SELECT `email`, `password` FROM `users` WHERE `email` = :email;");
+            $query->bindParam(':email', $userEmail);
+            $query->execute();
+            return $query->fetch();
+        }
+        return [];
+    }
+
+    /**
+     * Verifies user credentials by comparing form input with email and hashed password in database
+     */
+    public function userLoginVerify(string $userEmail, string $password, $userCredentials): bool
+    {
+        if (
+            (is_array($userCredentials)) &&
+            ($userEmail === $userCredentials['email']) &&
+            (password_verify($password, $userCredentials['password']))
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Inserts new user into database - registering.
+     */
+    public function insertNewUserToDb(string $registerEmail, string $registerPassword): bool
+    {
+        $query = $this->db->prepare(
+            "INSERT INTO `users` (`email`, `password`) VALUES (:email, :password);"
+        );
+        $query->bindParam(':email', $registerEmail);
+        $query->bindParam(':password', $registerPassword);
+        return $query->execute();
+    }
+
+    /** Validates if parameter is an email
+     *
+     * @return mixed returns the email as a string if its a valid email otherwise it returns false
+     */
+    private function validateEmail($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+}
