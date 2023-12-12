f1bd9362adbacf617fb020a5c8ffcb02cc08f6f7
Andrej <andrej.hagenmueller@gmail.com>
2023-12-12 15:52:49 +0000

    add CORS stuff

--- app/routes.php
+++ app/routes.php
@@ -7,17 +7,30 @@ use App\Controllers\LoginController;
 use App\Controllers\RegisterController;
 use App\Controllers\UserController;
 use Slim\App;
-use Slim\Views\PhpRenderer;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    $container = $app->getContainer();
+    // enable options requests
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
 
-    //demo code - two ways of linking urls to functionality, either via anon function or linking to a controller
+    // enable CORS
+    $app->add(function ($request, $handler) {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
 
-    $app->get('/', function ($request, $response, $args) use ($container) {
-        $renderer = $container->get(PhpRenderer::class);
-        return $renderer->render($response, "index.php", $args);
+    $app->get('/', function ($request, $response) {
+        $html = '<h1>Invoice system API</h1>
+                <p>To use this API please check the 
+                    <a href="https://github.com/iO-Academy/invoicing-API#api-documentation" target="_blank">documentation</a>.
+                </p>';
+        $response->getBody()->write($html);
+        return $response->withHeader('Content-type', 'text/html')->withStatus(200);
     });
 
     $app->get('/courses', CoursesAPIController::class);
