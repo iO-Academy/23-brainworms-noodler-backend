9a6c9106a8de71773458c3e003f669bb7de1a259
Claudia Lim <climweiching@gmail.com>
2023-12-13 14:11:14 +0000

    remove unneccessary files

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Controllers\CoursesAPIController;
 use App\Controllers\LoginController;
 use App\Controllers\RegisterController;
 use App\Controllers\UserController;
@@ -24,16 +23,6 @@ return function (App $app) {
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
 
-    $app->get('/', function ($request, $response) {
-        $html = '<h1>Invoice system API</h1>
-                <p>To use this API please check the 
-                    <a href="https://github.com/iO-Academy/invoicing-API#api-documentation" target="_blank">documentation</a>.
-                </p>';
-        $response->getBody()->write($html);
-        return $response->withHeader('Content-type', 'text/html')->withStatus(200);
-    });
-
-    $app->get('/courses', CoursesAPIController::class);
     $app->post('/login', LoginController::class);
     $app->post('/register', RegisterController::class);
     $app->get('/users/{id}', UserController::class);
